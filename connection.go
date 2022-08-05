ed24aa6fbe09b36d41e03287a77046d21d137ae7
PR <creature@localhost.localdomain>
2022-08-04 18:41:59 -0700

    docstring & validation

--- connection.go
+++ connection.go
@@ -6,20 +6,34 @@ import (
 )
 
 type Connection struct {
-	c Client
+	c                 Client
+	disableValidation bool
 }
 
+// CreateConnection will create a new synchronous http connection to the specified XRPL server address.
 func CreateConnection(address string) *Connection {
 	return &Connection{
 		c: http.NewClient(address),
 	}
 }
 
+// DisableValidation will disable parameter validation for all subsequent requests.
+func (c *Connection) DisableValidation() {
+	c.disableValidation = true
+}
+
+// EnableValidation will enable parameter validation for all subsequent requests. Enabled by default.
+func (c *Connection) EnableValidation() {
+	c.disableValidation = false
+}
+
 // Submit is a generic function to pass requests to connected XRPL Node
 // Response will be in the format associated with request parameters
 func (c *Connection) Submit(p types.XRPLParams) (types.XRPLResponse, error) {
-	if err := p.Valid(); err != nil {
-		return nil, err
+	if !c.disableValidation {
+		if err := p.Valid(); err != nil {
+			return nil, err
+		}
 	}
 	resp, err := c.c.Request(p.MethodString(), p)
 	if err != nil {
